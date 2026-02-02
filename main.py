18c07c5120bf4bbeb65d43b3e01eb510ebbb66bb
mrdoo248 <medm85211@gmail.com>
2026-02-02 09:07:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+# قائمة لحفظ آخر أمر
+last_cmd = {}
+
+@app.route("/")
 def home():
     return "Server is running ✅"
 
-@app.route("/echo", methods=["POST"])
-def echo():
-    data = request.get_json(silent=True)
-    return jsonify({
-        "received": data
-    })
+@app.route("/send", methods=["POST"])
+def send_cmd():
+    global last_cmd
+    data = request.json
+    last_cmd = data
+    print("📥 CMD received:", data)
+    return jsonify({"status": "ok", "received": data})
+
+@app.route("/receive", methods=["GET"])
+def receive_cmd():
+    return jsonify(last_cmd)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
